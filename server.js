52609df7dd7f56e0be0ffbf5052decf5b0d7fe2e
Kingdavid <kingigbayilola@gmail.com>
2024-07-05 11:34:38 +0100

    updated

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ import express from "express";
 const app = express();
 
 app.get("/", (req, res) => {
-  res.status(200).json({
+  return res.status(200).json({
     message: "Hello World",
   });
 });
