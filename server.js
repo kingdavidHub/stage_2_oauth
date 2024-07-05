0470695848733c3dad4e35d20cc03ebb290145ed
Kingdavid <kingigbayilola@gmail.com>
2024-07-05 11:23:54 +0100

    first commit

---
+++ server.js
@@ -0,0 +1,20 @@
+import dotenv from "dotenv";
+dotenv.config({path: `${process.cwd()}/.ENV`});
+import express from "express";
+
+
+const app = express();
+
+app.get("/", (req, res) => {
+  return res.status(200).json({
+    message: "Hello World",
+  });
+});
+
+
+const PORT  = process.env.SERVER_PORT || 6060;
+
+
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+});
