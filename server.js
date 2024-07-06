6442c749be3215fb47723f560b2a523e73afd657
Kingdavid <kingigbayilola@gmail.com>
2024-07-06 14:51:49 +0100

    test connection db

--- server.js
+++ server.js
@@ -5,11 +5,12 @@ import createRoute from "./routes/index.js";
 import userRoute from "./routes/user.js";
 import organizationRoute from "./routes/organisations.js";
 import morgan from "morgan";
+import db from "./db/index.js";
 
 const app = express();
 
 // Middleware
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
@@ -21,24 +22,34 @@ app.get("/", async (req, res) => {
 
 app.use("/auth", createRoute);
 app.use("/api/users", userRoute);
-app.use("/api/organisations", organizationRoute)
+app.use("/api/organisations", organizationRoute);
 
+// test endpoint
+app.get("/test", (req, res) => {
+  try {
+    const { rows } = db.query("SELECT * FROM users");
+    return res.status(200).json({
+      data: rows,
+    });
+  } catch (error) {
+    res.status(500).json({ error });
+  }
+});
 
 app.get("*", (req, res) => {
   return res.status(404).json({
     message: "Route not found",
-  })
-})
-
+  });
+});
 
 // global error handler
 app.use((err, req, res, next) => {
   console.log(err);
   res.status(500).json({
-    message: err.message
-  })
+    message: err.message,
+  });
   next();
-})
+});
 
 const PORT = process.env.SERVER_PORT || 6060;
 
