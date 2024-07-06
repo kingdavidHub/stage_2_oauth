751ead097dc2b6717193ee208446081520093b90
Kingdavid <kingigbayilola@gmail.com>
2024-07-06 14:46:44 +0100

    updated db connection for async

--- db/index.js
+++ db/index.js
@@ -1,17 +1,23 @@
 import dotenv from "dotenv";
-dotenv.config({ path: `${process.cwd()}/.ENV` });
 import pg from "pg";
+
+dotenv.config({ path: `${process.cwd()}/.ENV` });
+
 const { Pool } = pg;
-
-// lOCAL DB
-// const pool = new Pool();
-
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
 });
 
-
+const query = async (text, params) => {
+  try {
+    const result = await pool.query(text, params);
+    return result;
+  } catch (error) {
+    console.error("Error executing query:", error.message);
+    throw error;
+  }
+};
 
 export default {
-  query: (text, params) => pool.query(text, params),
+  query,
 };
