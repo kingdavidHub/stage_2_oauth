602bbbaffee468326efa7c26eb12ae60e033f0c2
Kingdavid <kingigbayilola@gmail.com>
2024-07-08 00:55:20 +0100

    updated login

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,10 @@ import { v4 as uuid } from "uuid";
 import db from "../db/index.js";
 import authorizeToken from "../middleware/authorizeToken.js";
 import generateToken from "../utils/generateToken.js";
-import { login_validation, register_validation } from "../middleware/combinedValidation.js";
+import {
+  login_validation,
+  register_validation,
+} from "../middleware/combinedValidation.js";
 import { matchedData } from "express-validator";
 
 // console.log(uuid());
@@ -12,14 +15,18 @@ import { matchedData } from "express-validator";
 const router = express.Router();
 
 router.post("/register", register_validation, async (req, res) => {
-  
-  const { firstName, lastName, email, password: plainTextPassword } = matchedData(req);
+  const {
+    firstName,
+    lastName,
+    email,
+    password: plainTextPassword,
+  } = matchedData(req);
   const { phone } = req.body;
   const orgName = `${firstName}'s Organization`;
 
-  const saltRounds = 10
+  const saltRounds = 10;
   const salt = bcrypt.genSaltSync(saltRounds);
-  const hashPassword =bcrypt.hashSync(plainTextPassword, salt);
+  const hashPassword = bcrypt.hashSync(plainTextPassword, salt);
 
   try {
     //Create user
@@ -72,19 +79,22 @@ router.post("/register", register_validation, async (req, res) => {
       status: "Internal server error",
       message: error.message,
       statusCode: 500,
-    })
+    });
   }
 });
 
-router.post("/login",login_validation, async (req, res) => {
-  const { email, password: plainTextPassword } = req.body;
+router.post("/login", login_validation, async (req, res) => {
+  const { email, password: plainTextPassword } = matchedData(req);
 
   try {
     const { rows } = await db.query("SELECT * FROM users WHERE email = $1", [
       email,
     ]);
 
-    if (!(rows.length > 0) || (!bcrypt.compareSync(plainTextPassword, rows[0].password))) {
+    if (
+      !(rows.length > 0) ||
+      !bcrypt.compareSync(plainTextPassword, rows[0].password)
+    ) {
       return res.status(401).json({
         status: "Bad request",
         message: "Authentication failed",
@@ -112,7 +122,7 @@ router.post("/login",login_validation, async (req, res) => {
       status: "Internal server error",
       message: error.message,
       statusCode: 500,
-    })
+    });
   }
 });
 
