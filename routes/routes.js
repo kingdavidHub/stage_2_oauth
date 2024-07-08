e102794973eb2265ea4d71f48c6c77231ff41cd1
David Igbayilola <kingigbayilola@gmail.com>
2024-07-08 14:51:46 +0100

    Update routes.js

--- routes/routes.js
+++ routes/routes.js
@@ -1,12 +1,12 @@
 import express from "express";
 import createRoute from "../routes/index.js";
-//import userRoute from "../routes/user.js";
+import userRoute from "../routes/user.js";
 import organizationRoute from "../routes/organisations.js";
 
 const router = express.Router();
 
 router.use("/auth", createRoute);
-//router.use("/users", userRoute);
+router.use("/users", userRoute);
 router.use("/organisations", organizationRoute);
 
 export default router;
