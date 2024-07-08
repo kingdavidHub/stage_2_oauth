7a02b5e6d3d54d75c8d758a68902e3140d3ae677
David Igbayilola <kingigbayilola@gmail.com>
2024-07-08 14:29:24 +0100

    Update user.js

--- routes/user.js
+++ routes/user.js
@@ -13,12 +13,16 @@ router.get("/:id", authorizeToken, async (req, res) => {
 
     delete rows[0].password
 
-
-
     return res.status(200).json({
       status: "success",
       message: "User retrieved successfully",
-      data: rows[0],
+      data: {
+        userId: rows[0].userid,
+        firstName: rows[0].firstname,
+        lastName: rows[0].lastname,
+        email: rows[0].email,
+        phone: rows[0].phone,
+        
     });
   } catch (error) {
     console.log(error);
